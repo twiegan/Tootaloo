e133806d2be59e00989fd3e2d965fe285f4110eb
Samuel Tarr <54817476+S-Tarr@users.noreply.github.com>
2023-04-24 18:56:21 -0400

    fix required fields

--- tootaloo/lib/ui/screens/searches/restroom_search_screen.dart
+++ tootaloo/lib/ui/screens/searches/restroom_search_screen.dart
@@ -38,6 +38,7 @@ class _RestroomSearchScreenState extends State<RestroomSearchScreen> {
       cleanliness: 0.0,
       internet: 0.0,
       vibe: 0.0,
+      privacy: 0.0,
       ratings_ids: []);
   // late List<RestroomTileItem> _restroomTiles = [];
 
@@ -276,6 +277,7 @@ Future<List<Rating>> getRating(List<String> ids) async {
         cleanliness: rating["cleanliness"],
         internet: rating["internet"],
         vibe: rating["vibe"],
+        privacy: rating["privacy"],
         upvotes: rating["upvotes"],
         downvotes: rating["downvotes"],
         review: rating["review"],
